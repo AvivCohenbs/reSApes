89f81847ca396c63193bd169efa8d64643a1417c
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2021-12-15 20:55:06 +0200

    add profile image and add route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import "./Header.css";
 import { ReactComponent as Ingredients } from "./ingredients.svg";
+import { ReactComponent as Profile } from "./Profile.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/Profile">
+        <Profile />
+      </Link>
       <div className="titles">
         <div className="main-title">Hello, </div>
         <div className="second-title"> Let's start cooking</div>
