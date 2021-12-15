4b8a1f8e3ffde9f1299178d5f1f3c57cda5b80e6
Shir Ram <shirram54@gmail.com>
2021-12-15 12:13:53 +0200

    popular recipes done

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,8 +2,9 @@ import "./Recipe.css";
 // import { Link } from "react-router-dom";
 import { ReactComponent as Button } from "./button.svg";
 import { ReactComponent as Rating } from "./rating.svg";
+import { ReactComponent as Clock } from "./clock.svg";
 
-function Recipe({ title, description, time, image }) {
+function Recipe({ title, description, time, image, rating }) {
   return (
     <div className="recipe-card">
       <div className="recipe-image">
@@ -15,11 +16,14 @@ function Recipe({ title, description, time, image }) {
         <h3 className="recipe-title">{title}</h3>
         {/* </Link> */}
         <h5 className="desc">{description}</h5>
-        <h6 className="time">{time}</h6>
+        <h6 className="time">
+          <Clock />
+          <span className="minute">{time}</span>
+        </h6>
       </div>
       <div className="info">
         <h6 className="star">
-          <Rating />
+          <Rating /> {rating}
         </h6>
         <button className="btn-start">
           <Button />{" "}
