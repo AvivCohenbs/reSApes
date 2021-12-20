33d4ecc9a430ace6a1a6bb689755026d553a3fde
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2021-12-20 21:15:33 +0200

    started favorites page - header and user icon

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,17 @@
 import "./Favorites.css";
+import { ReactComponent as ProfileGreen } from "./ProfileGreen.svg";
+import { Link } from "react-router-dom";
 
 function Favorites() {
-  return <div></div>;
+  return (
+    <div className="bookmark-header">
+      <div className="main-title">My Favorites</div>
+      <div className="user-favorites">
+        <Link to="/Profile">
+          <ProfileGreen />
+        </Link>
+      </div>
+    </div>
+  );
 }
 export default Favorites;
