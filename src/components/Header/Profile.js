e5bb55593bdbe4f7c1eb72923c4f4d5e15713844
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2021-12-22 20:52:58 +0200

    started design profile page

--- src/components/Header/Profile.js
+++ src/components/Header/Profile.js
@@ -1,6 +1,27 @@
 import "./Profile.css";
+import { ReactComponent as Singin } from "./singin.svg";
 
 function Profile() {
-  return <div></div>;
+  return (
+    <div className="profile-page">
+      <div className="sing-In">Sign In Now</div>
+      <div className="login">
+        <form className="login-email">
+          <label>
+            <input type="text" placeholder="Email" />
+            <Singin className="iconMan" />
+          </label>
+        </form>
+        <form>
+          <div class="login-pas">
+            <input type="password" placeholder="Password" />
+          </div>
+        </form>
+      </div>
+      <div className="container">
+        <button className="btn">Submit</button>
+      </div>
+    </div>
+  );
 }
 export default Profile;
