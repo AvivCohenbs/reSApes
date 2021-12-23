861b2d38a929c67d747aff412113a087c1b78217
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2021-12-23 11:54:52 +0200

    hide and show pssword pic

--- src/components/Header/Profile.js
+++ src/components/Header/Profile.js
@@ -1,7 +1,13 @@
 import "./Profile.css";
 import { ReactComponent as Singin } from "./singin.svg";
+import React, { useState } from "react";
+import { ReactComponent as showpsd } from "./showpsd.svg";
+import { ReactComponent as hidepsd } from "./hidepsd.svg";
 
 function Profile() {
+  const [pwd, setPwd] = useState("");
+  const [isRevealPwd, setIsRevealPwd] = useState(false);
+
   return (
     <div className="profile-page">
       <div className="sing-In">Sign In Now</div>
@@ -12,11 +18,22 @@ function Profile() {
             <Singin className="iconMan" />
           </label>
         </form>
-        <form>
-          <div class="login-pas">
-            <input type="password" placeholder="Password" />
-          </div>
-        </form>
+        <div className="pwd-container">
+          <hidePwdImg />
+          <showPwdImg />
+          <input
+            name="pwd"
+            placeholder="Enter Password"
+            type={isRevealPwd ? "text" : "password"}
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+          <img
+            title={isRevealPwd ? "Hide password" : "Show password"}
+            src={isRevealPwd ? hidepsd : showpsd}
+            onClick={() => setIsRevealPwd((prevState) => !prevState)}
+          />
+        </div>
       </div>
       <div className="container">
         <button className="btn">Submit</button>
