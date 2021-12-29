c2a786ed470622ce1e6c7e9f4b30151cf42e3816
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2021-12-29 20:13:59 +0200

    change class name and add profile pic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,21 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 import React from "react";
+import { ReactComponent as Profile } from "./Profile.svg";
 
 function Header() {
   return (
     <div className="header">
-      <Link className="Logo" to="/">
+      <Link className="logo" to="/">
         <div>reSApes</div>
       </Link>
       <input class="menu-btn" type="checkbox" id="menu-btn" />
       <label class="menu-icon" for="menu-btn">
         <span class="navicon"></span>
       </label>
-
+      <Link className="signup" to="/Profile">
+        <Profile />
+      </Link>
       <ul className="menu">
         <li>
           {" "}
@@ -23,11 +26,6 @@ function Header() {
         </li>
         <li>
           <Link to="/Profile">Community</Link>
-        </li>
-
-        <li>
-          {" "}
-          <Link to="/Profile">Sign Up</Link>
         </li>
       </ul>
     </div>
