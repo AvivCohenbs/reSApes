5f75ab023875dc613451fe9360d6b54afe7dc4e1
Shir Ram <shirram54@gmail.com>
2022-01-05 22:21:08 +0200

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,19 +12,6 @@ function Header() {
         <Logo />
       </Link>
       {/* <input class="menu-btn" type="checkbox" id="menu-btn" /> */}
-      <div className="menu-icons">
-        {/* <label class="menu-icon" for="menu-btn">
-          <span class="navicon"></span>
-        </label> */}
-        <div className="lefticones">
-          <Link className="bell" to="/notes">
-            <Bell />
-          </Link>
-          <Link className="signup" to="/Profile">
-            <Profile />
-          </Link>
-        </div>
-      </div>
       <ul className="menu">
         <li>
           <Link to="/Favorites">My recipes</Link>{" "}
@@ -39,6 +26,14 @@ function Header() {
           <Link to="/About">About</Link>{" "}
         </li>
       </ul>
+      <div className="lefticones">
+        <Link className="bell" to="/notes">
+          <Bell />
+        </Link>
+        <Link className="signup" to="/Profile">
+          <Profile />
+        </Link>
+      </div>
     </div>
   );
 }
