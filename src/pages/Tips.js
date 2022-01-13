5d0504c97673c923437bb49db6baa46f3bf62d31
Shir Ram <shirram54@gmail.com>
2022-01-13 15:02:22 +0200

    tips css

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -1,5 +1,5 @@
 import "./Tips.css";
-import { ReactComponent as Halaa } from "./Halaa.svg";
+import { ReactComponent as Filo } from "./Filo.svg";
 import { ReactComponent as Tableimg } from "./Tableimg.svg";
 
 function Tips() {
@@ -11,13 +11,17 @@ function Tips() {
       <div className="img-salad">
         <Tableimg />
       </div>
+
+      <div className="p-1">
+        When you love food, it does not mean that you have to be addicted to the
+        kitchen.
+        <br />
+        you always have to have the right products in the pantry and a few
+        tricks up your sleeve.
+        <br />
+        This is how we are hosted effortlessly
+      </div>
       <div className="p">
-        <div className="p-1">
-          When you love food, it does not mean that you have to be addicted to
-          the kitchen: you always have to have the right products in the pantry
-          and a few tricks up your sleeve. This is how we are hosted
-          effortlessly
-        </div>
         <div className="p-2">
           Hospitality with ready-made food has a slightly dubious image, but it
           does not have to be this way - if done right, one can entertain, serve
@@ -65,70 +69,89 @@ function Tips() {
           dessert.
         </div>
         <div className="p-7">2. Equip the pantry</div>
-        <div>
+        <div className="p-8">
           The fresh raw materials like cheeses, vegetables, fruits and sausages
           are specially bought, depending on the concept chosen. The others
           should be added to the pantry or freezer regardless of planned
           accommodation - they can also shorten the way to the dishes you
           prepare on a daily basis.
         </div>
-        <div className="p-8">
-          Cheeses- Cheeses can be incorporated into almost any concept and any
-          meal. The obvious is cheese-based hospitality - to which only breads
-          or crackers, fruit and nuts are added - but cheeses can do much more
-          than that. Here are some examples:
+        <div className="p-9">
+          <span className="span"> Cheeses </span> - can be incorporated into
+          almost any concept and any meal. The obvious is cheese-based
+          hospitality - to which only breads or crackers, fruit and nuts are
+          added - but cheeses can do much more than that. Here are some
+          examples:
         </div>
-        <div>
-          - You can design a table with different types of bruschettas, each of
-          which has a different topping with a different cheese, for example a
-          broccoli pesto broccoli and a king of the forest steak with parmesan,
-          ratatouille bruschetta and smoked gouda; Bruschetta with gouda tom
-          goats and artichokes etc.
-          <br />
-          - Cheeses can also be at the center of light hospitality dishes, ones
-          that have 3-4 quality ingredients, for example stewed asparagus topped
-          with smoked Gouda carvings; Roasted beets with blue cheese and nuts;
-          Halved figs with goat cheese and a drizzle of honey, a loaf of bread
-          that make grooves in it and fill them with various hard cheeses and
-          crushed garlic cloves or spreads like pesto and sun-dried tomatoes or
-          hard cheese cubes seasoned with olive oil and hot pepper or sun-dried
-          tomatoes and served as a snack.
-          <br />- The addition of cheese can also give an invested touch to
-          everyday dishes - for example fried diced cubes on a green salad (the
-          lazy can make it from a purchased washed leaf mixture) or diced
-          Bulgarian cheese on a simple green salad. On the same principle, you
-          can spread sheep or goat yogurt on a nice serving plate, season with a
-          little hyssop, blush or paprika that will add color and thus serve a
-          lentil or cereal salad.
+        <div className="p-10">
+          <ul>
+            <li>
+              You can design{" "}
+              <span className="span">
+                {" "}
+                table with different types of bruschettas
+              </span>
+              , each of which has a different topping with a different cheese,
+              for example a broccoli pesto broccoli and a king of the forest
+              steak with parmesan, ratatouille bruschetta and smoked gouda;
+              Bruschetta with gouda tom goats and artichokes etc.
+            </li>
+            <li>
+              Cheeses can also be at the center of light hospitality dishes,
+              ones that have 3-4 quality ingredients, for example stewed
+              asparagus topped with smoked Gouda carvings; Roasted beets with
+              blue cheese and nuts; Halved figs with goat cheese and a drizzle
+              of honey, a loaf of bread that make grooves in it and fill them
+              with various hard cheeses and crushed garlic cloves or spreads
+              like pesto and sun-dried tomatoes or hard cheese cubes seasoned
+              with olive oil and hot pepper or sun-dried tomatoes and served as
+              a snack.
+            </li>
+            <li>
+              {" "}
+              The addition of cheese can also give an invested touch to everyday
+              dishes - for example fried diced cubes on a green salad (the lazy
+              can make it from a purchased washed leaf mixture) or diced
+              Bulgarian cheese on a simple green salad. On the same principle,
+              you can spread sheep or goat yogurt on a nice serving plate,
+              season with a little hyssop, blush or paprika that will add color
+              and thus serve a lentil or cereal salad.
+            </li>
+          </ul>
         </div>
-        {/* <div className="img-hala">
-        <Halaa />
-      </div> */}
-        <div>
-          Salads and spreads such as pesto, artichoke spread, sun-dried tomato
-          spread, egg salad, eggplant salad and more. You can place them on a
-          buffet table for spreading, or use them as an ingredient in
+      </div>
+      <div className="img-bread">
+        <Filo />
+      </div>
+      <div className="pp">
+        <div className="p-11">
+          <b>Salads and spreads</b> such as pesto, artichoke spread, sun-dried
+          tomato spread, egg salad, eggplant salad and more. You can place them
+          on a buffet table for spreading, or use them as an ingredient in
           bruschettas or quick pastries. Either way, you do not have to buy
           dozens of salads - focus on 4-3 that fit the concept you have chosen,
           but invest in salads from a quality deli and not from an unknown brand
           that you found in the supermarket.
         </div>
-        <div>
-          Quality preserves The base is olives and pickles of various kinds, but
-          you can expand the canvas with gherkins, capers (small and large) and
-          artichokes a la Romana and even find in deli delicacies also less
-          routine preserves, for example patissons, pickled peppers stuffed with
-          cheese or garlic confit. And of course, if the menu is appropriate -
-          to this category you can also add pickled and smoked fish.
+        <div className="p-12">
+          <b>Quality preserves</b> The base is olives and pickles of various
+          kinds, but you can expand the canvas with gherkins, capers (small and
+          large) and artichokes a la Romana and even find in deli delicacies
+          also less routine preserves, for example patissons, pickled peppers
+          stuffed with cheese or garlic confit. And of course, if the menu is
+          appropriate - to this category you can also add pickled and smoked
+          fish.
         </div>
-        <div>
-          Spices and spice mixes Beautiful saucers with coarse salt, ground
-          black pepper and a spice mixture that is suitable for cheeses or
-          vegetables will also add to the variety on the table and will also add
-          interest to the dishes.
+        <div className="p-13">
+          <b>Spices and spice mixes</b> Beautiful saucers with coarse salt,
+          ground black pepper and a spice mixture that is suitable for cheeses
+          or vegetables will also add to the variety on the table and will also
+          add interest to the dishes.
         </div>
-        <div>3. Winning pair - creative uses for doughs and cheeses</div>
-        <div>
+        <div className="p-14">
+          3. Winning pair - creative uses for doughs and cheeses
+        </div>
+        <div className="p-15">
           Ready-made doughs are the basis for countless games and impressive
           hospitality dishes. All you have to do is thaw them according to the
           manufacturer's instructions, mix them with vegetables and cheeses and
