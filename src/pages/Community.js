4b583d628338cc9c19e84f8ae8b5822d442ad4eb
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2022-01-17 15:34:14 +0200

    add backgound color and started community page

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,6 +1,10 @@
 import "./Community.css";
 
 function Community() {
-  return <div></div>;
+  return (
+    <div className="addnew">
+      <div className="newrecipe">Share Your Favorite Recipe With Us</div>
+    </div>
+  );
 }
 export default Community;
