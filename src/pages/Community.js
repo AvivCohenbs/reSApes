d2056216ab583b6951f31f4314c8722a62605d0c
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2022-01-18 10:50:31 +0200

    add titles for community page

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -3,7 +3,10 @@ import "./Community.css";
 function Community() {
   return (
     <div className="addnew">
-      <div className="newrecipe">Share Your Favorite Recipe With Us</div>
+      <div className="maintitle">
+        <h1 className="firsttitle">Share Your Favorite Recipe With Us</h1>
+        <h1 className="sectitle">Add New Recipe</h1>
+      </div>
     </div>
   );
 }
