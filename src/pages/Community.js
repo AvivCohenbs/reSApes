d8a7f26f1015e144c2a8d4c3f6e24ee42cb8df7a
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2022-01-26 19:39:35 +0200

    changed comm

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -13,37 +13,42 @@ function Community() {
             <div className="form-col">
               <div className="form__item">
                 <label className="recipe__name" for="recipe-name">
-                  Recipe Name<i className="red">( max length 200 )</i>
+                  Recipe Name<i className="max">( max length 200 )</i>
                 </label>
-                <input name="recipe-name" />
+                <input className="recipe-name" />
               </div>
               <div className="form__item">
-                <label for="short-desc">Short Description</label>
-                <textarea name="short-desc" cols="30" rows="5"></textarea>
+                <label className="short-des">Description</label>
+                <textarea className="short-desc" cols="30" rows="5"></textarea>
               </div>
-              <div className="form__item">
-                <label for="ing-list">Ingredients</label>
-                <textarea name="ing-list" cols="30" rows="10"></textarea>
-              </div>
+              <div className="ing-list">Which ingredients do you have?</div>
             </div>
             <div className="form-col">
               <div className="row">
-                <div className="form__item">
+                {/* <div className="form__item">
                   <label className="recipe__time" for="prep_time">
                     Prep Time
                   </label>
                   <input className="time__input" type="text" />
-                </div>
+                </div> */}
                 <div className="form__item">
-                  <label className="recipe__time">Cooking Time</label>
-                  <input className="time__input" type="text" />
+                  <div className="recipe__time">Cooking Time</div>
+                  {/* <input className="time__input" type="text" /> */}
+                  <input
+                    type="time"
+                    id="appt"
+                    name="appt"
+                    min="09:00"
+                    max="18:00"
+                    required
+                  ></input>
                 </div>
-                <div className="form__item">
+                {/* <div className="form__item">
                   <label className="recipe__time" for="servings">
                     Servings
                   </label>
                   <input className="time__input" for="servings" type="text" />
-                </div>
+                </div> */}
               </div>
               <div className="form__item">
                 <label for="direction">Directions</label>
