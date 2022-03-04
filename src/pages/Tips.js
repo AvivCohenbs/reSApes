7278e2826bbd3c0b2b1a3dda66ff1f800457729f
Shir Ram <shirram54@gmail.com>
2022-03-04 15:12:51 +0200

    css

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -35,16 +35,23 @@ function Tips() {
                 <AccordionDetails sx={{ backgroundColor: "#fefbf3" }}>
                   <Typography>
                     <ul className="paragraph">
-                      <li>Keep it minimal</li>
-                      <li>Start with a clean kitchen</li>
-                      <li>Clean while you wait</li>
-                      <li>Let the water out of the sink right away</li>
-                      <li>Wipe out the sink after washing dishes</li>
-                      <li> Clean up spills ASAP</li>
-                      <li>Clean as you go</li>
-                      <li>Have a compost/trash bowl</li>
-                      <li>A place for everything</li>
-                      <li> Have set days and times for doing things</li>
+                      <li className="li-tips">Keep it minimal</li>
+                      <li className="li-tips">Start with a clean kitchen</li>
+                      <li className="li-tips">Clean while you wait</li>
+                      <li className="li-tips">
+                        Let the water out of the sink right away
+                      </li>
+                      <li className="li-tips">
+                        Wipe out the sink after washing dishes
+                      </li>
+                      <li className="li-tips"> Clean up spills ASAP</li>
+                      <li className="li-tips">Clean as you go</li>
+                      <li className="li-tips">Have a compost/trash bowl</li>
+                      <li className="li-tips">A place for everything</li>
+                      <li className="li-tips">
+                        {" "}
+                        Have set days and times for doing things
+                      </li>
                     </ul>
                   </Typography>
                 </AccordionDetails>
@@ -67,14 +74,24 @@ function Tips() {
                     <div className="paragraph">
                       {" "}
                       <ul className="paragraph">
-                        <li>Start with one or two basic cookbooks</li>
-                        <li>Keep the fancy equipment to a minimum</li>
-                        <li>Don't go off-recipe until you're more confident</li>
-                        <li>Good knives are a must</li>
-                        <li>Start a collection of herbs and spices</li>
-                        <li>Cook in bulk</li>
-                        <li>Invest in a set of basic pans</li>
-                        <li>Don't get frustrated</li>
+                        <li className="li-tips">
+                          Start with one or two basic cookbooks
+                        </li>
+                        <li className="li-tips">
+                          Keep the fancy equipment to a minimum
+                        </li>
+                        <li className="li-tips">
+                          Don't go off-recipe until you're more confident
+                        </li>
+                        <li className="li-tips">Good knives are a must</li>
+                        <li className="li-tips">
+                          Start a collection of herbs and spices
+                        </li>
+                        <li className="li-tips"> Cook in bulk</li>
+                        <li className="li-tips">
+                          Invest in a set of basic pans
+                        </li>
+                        <li className="li-tips">Don't get frustrated</li>
                       </ul>
                     </div>
                   </Typography>
@@ -97,17 +114,25 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>
+                        <li className="li-tips">
                           Take one minute to mentally walk through what you're
                           cooking
                         </li>
-                        <li>Set up appliances and heat the oven</li>
-                        <li>Get the water boiling immediately</li>
-                        <li>
+                        <li className="li-tips">
+                          Set up appliances and heat the oven
+                        </li>
+                        <li className="li-tips">
+                          Get the water boiling immediately
+                        </li>
+                        <li className="li-tips">
                           Load a pan with ingredients from the pantry or fridge
                         </li>
-                        <li>Clean your produce efficiently</li>
-                        <li>Figure out your prepping order and multitask</li>
+                        <li className="li-tips">
+                          Clean your produce efficiently
+                        </li>
+                        <li className="li-tips">
+                          Figure out your prepping order and multitask
+                        </li>
                       </ul>
                     </div>
                   </Typography>
@@ -155,15 +180,15 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>
+                        <li className="li-tips">
                           Store raw meat and poultry in clean, sealed containers
                           on the bottom shelf of the fridge
                         </li>
-                        <li>
+                        <li className="li-tips">
                           Follow any storage instructions on the label and do
                           not eat meat after its use by date
                         </li>
-                        <li>
+                        <li className="li-tips">
                           Keep cooked meat separate from raw meat and
                           ready-to-eat foods in general
                         </li>
@@ -189,18 +214,26 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>
+                        <li className="li-tips">
                           If you don't plan to eat the fish within a couple of
                           days, freeze it instead
                         </li>
-                        <li>Remove the fish from the store packaging</li>
-                        <li>
+                        <li className="li-tips">
+                          Remove the fish from the store packaging
+                        </li>
+                        <li className="li-tips">
                           Rinse it under cold water and use paper towels to dry
                           it
                         </li>
-                        <li>Put the fish in freezer bags or containers</li>
-                        <li>Label and date the bags or containers</li>
-                        <li>Put in a freezer set at zero degrees or colder</li>
+                        <li className="li-tips">
+                          Put the fish in freezer bags or containers
+                        </li>
+                        <li className="li-tips">
+                          Label and date the bags or containers
+                        </li>
+                        <li className="li-tips">
+                          Put in a freezer set at zero degrees or colder
+                        </li>
                       </ul>
                     </div>
                   </Typography>
@@ -222,29 +255,31 @@ function Tips() {
                 <AccordionDetails sx={{ backgroundColor: "#fefbf3" }}>
                   <Typography>
                     <ul className="paragraph">
-                      <li>
+                      <li className="li-tips">
                         {" "}
                         To create an egg wash, whisk together a large egg with
                         one tablespoon of water until smooth. Use as a glue to
                         seal pastries, then brush on top for a glossy appearance
                       </li>
-                      <li>Learn all the different ways to cook an egg</li>
-                      <li>
+                      <li className="li-tips">
+                        Learn all the different ways to cook an egg
+                      </li>
+                      <li className="li-tips">
                         When poaching an egg, add a teaspoon of white vinegar to
                         simmering water to help keep the yolk from breaking
                       </li>
-                      <li>
+                      <li className="li-tips">
                         {" "}
                         For a great hardboiled egg every time, bring your pot to
                         a boil and then turn off the heat. Let your eggs sit in
                         the heated pot for 12 minutes and then transfer to cold
                         water
                       </li>
-                      <li>
+                      <li className="li-tips">
                         Crack eggs on a paper towel on the counter — no shells
                         and easy cleanup!
                       </li>
-                      <li>
+                      <li className="li-tips">
                         Make an ideal sunny-side egg by covering your pan with a
                         lid and letting the steam cook your egg. No flipping
                         required
@@ -271,17 +306,23 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>Use a big enough pot</li>
-                        <li>Use plenty of water</li>
-                        <li>Season the pasta water</li>
-                        <li>Let the water come to a boil first</li>
-                        <li>Stir right away — and every couple minutes</li>
-                        <li>
+                        <li className="li-tips">Use a big enough pot</li>
+                        <li className="li-tips">Use plenty of water</li>
+                        <li className="li-tips">Season the pasta water</li>
+                        <li className="li-tips">
+                          Let the water come to a boil first
+                        </li>
+                        <li className="li-tips">
+                          Stir right away — and every couple minutes
+                        </li>
+                        <li className="li-tips">
                           Don't rely solely on the cooking time on your pasta's
                           package
                         </li>
-                        <li>Cook your pasta to just before al dente</li>
-                        <li>Reserve the pasta water</li>
+                        <li className="li-tips">
+                          Cook your pasta to just before al dente
+                        </li>
+                        <li className="li-tips">Reserve the pasta water</li>
                       </ul>
                     </div>
                   </Typography>
@@ -305,14 +346,16 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>Chill your onions</li>
-                        <li>Fan away the fumes</li>
-                        <li>Cut under cold water</li>
-                        <li>Sharpen that knife</li>
-                        <li>Work on your knife skills</li>
-                        <li>Cut near an open flame</li>
-                        <li>Cut off the top of the onion</li>
-                        <li>Wear goggles</li>
+                        <li className="li-tips">Chill your onions</li>
+                        <li className="li-tips">Fan away the fumes</li>
+                        <li className="li-tips">Cut under cold water</li>
+                        <li className="li-tips">Sharpen that knife</li>
+                        <li className="li-tips">Work on your knife skills</li>
+                        <li className="li-tips">Cut near an open flame</li>
+                        <li className="li-tips">
+                          Cut off the top of the onion
+                        </li>
+                        <li className="li-tips">Wear goggles</li>
                       </ul>
                     </div>
                   </Typography>
@@ -337,32 +380,36 @@ function Tips() {
                   <Typography>
                     <div className="paragraph">
                       <ul>
-                        <li>
+                        <li className="li-tips">
                           Turn down the heat. Once you get used to your pots and
                           stove top you’ll realize that “medium-high” only needs
                           to be at “medium” on your stove top for the same
                           results — especially on electric stove tops and if you
                           let the pot heat up quickly
                         </li>
-                        <li>
+                        <li className="li-tips">
                           Practice with what you’re cooking. When you cook
                           something, actually record how long you’re cooking it.
                           If it burns, don’t cook it so long next time
                         </li>
-                        <li>Stir things regularly, as the recipe calls for</li>
-                        <li>
+                        <li className="li-tips">
+                          Stir things regularly, as the recipe calls for
+                        </li>
+                        <li className="li-tips">
                           Use cooking oils, olive oil is great for transferring
                           the heat to the food and adding flavor
                         </li>
-                        <li>
+                        <li className="li-tips">
                           Cover the pot and check it more often. When you cover
                           the pot it can help cook things quicker by keeping the
                           heat and steam in, it’ll cook more of the food that is
                           not in contact with the stove, so you won’t have to
                           cook things as long to get it cooked thoroughly
                         </li>
-                        <li>Cook smaller things. it cooks better</li>
-                        <li>
+                        <li className="li-tips">
+                          Cook smaller things. it cooks better
+                        </li>
+                        <li className="li-tips">
                           Use the oven, too. Before you get started, preheat the
                           oven to 425 or so. Sear the outsides of the food on
                           medium-high for about 3–5 minutes per side. Then
